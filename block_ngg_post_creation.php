55f1cb1e09ea431f8b72b39f1959ec2c05530ef9
TweetPressFr <tweetpressfr@gmail.com>
2014-05-27 03:55:15 +0200

    small fixes
    avoid fatal error with C function is_class ^^, avoid fatal errors with
    class name too much generic

--- block_ngg_post_creation.php
+++ block_ngg_post_creation.php
@@ -7,63 +7,66 @@ Author: Lionel Pointet
 Version: 1.0
 */
 
-class BPC {
-    /**
-     * Register needed hooks.
-     */
-    public static function hooks() {
-        add_action( 'init', array( __CLASS__, 'init' ) );
-    }
+if( !class_exists('BPC') ) {
 
-    /**
-     * Run at the 'init' hook.
-     *
-     * This function will disable post creation ability for all post types
-     */
-    public static function init() {
-        // Don't use is_plugin_active because :
-        //  1. it doesn't exist on front
-        //  2. it implies that we know the plugin directory
+	class BPC {
+		/**
+		 * Register needed hooks.
+		 */
+		public function hooks() {
+			add_action( 'init', array( __CLASS__, 'init' ) );
+		}
 
-        if( !is_class( 'C_NextGEN_Bootstrap' ) )
-            return;
+		/**
+		 * Run at the 'init' hook.
+		 *
+		 * This function will disable post creation ability for all post types
+		 */
+		public static function init() {
+			// Don't use is_plugin_active because :
+			//  1. it doesn't exist on front
+			//  2. it implies that we know the plugin directory
 
-        // NextGen Gallery plugin is active
-        // => disable post creation for each registered post type
+			if( !class_exists(  'C_NextGEN_Bootstrap' ) )
+				return;
 
-        $post_types = apply_filters( 'bpc_post_types', get_post_types( array(
-            'public' => true,
-        ) ) );
+			// NextGen Gallery plugin is active
+			// => disable post creation for each registered post type
 
-        foreach( $post_types as $post_type ) {
-            // Little tweak: for media post type, use 'map_meta_cap' filter instead
-            if( 'media' === $post_type ) {
-                add_filter( 'map_meta_cap', array( __CLASS__, 'map_meta_cap' ), 10, 2 );
-                continue;
-            }
+			$post_types = apply_filters( 'bpc_post_types', get_post_types( array(
+				'public' => true,
+			) ) );
 
-            $post_type_object = get_post_type_object( $post_type );
-            $post_type_object->cap->create_posts = 'do_not_allow';
-        }
-    }
+			foreach( $post_types as $post_type ) {
+				// Little tweak: for media post type, use 'map_meta_cap' filter instead
+				if( 'media' === $post_type ) {
+					add_filter( 'map_meta_cap', array( __CLASS__, 'map_meta_cap' ), 10, 2 );
+					continue;
+				}
 
-    /**
-     * Map meta capabilities to primitive capabilities.
-     *
-     * This function is used for 'media' post type, for which 'upload_files'
-     * capability is used directly to check the ability to create a media.
-     * This will return 'do_not_allow' cap if needed.
-     *
-     * @param string $caps Actual capabilities for meta capability.
-     * @param string $cap Capability name.
-     * @return array Actual capabilities for meta capability.
-     */
-    public static function map_meta_cap( $caps, $cap ) {
-        if( 'upload_files' !== $cap )
-            return $caps;
+				$post_type_object = get_post_type_object( $post_type );
+				$post_type_object->cap->create_posts = 'do_not_allow';
+			}
+		}
 
-        return array( 'do_not_allow' );
-    }
+		/**
+		 * Map meta capabilities to primitive capabilities.
+		 *
+		 * This function is used for 'media' post type, for which 'upload_files'
+		 * capability is used directly to check the ability to create a media.
+		 * This will return 'do_not_allow' cap if needed.
+		 *
+		 * @param string $caps Actual capabilities for meta capability.
+		 * @param string $cap Capability name.
+		 * @return array Actual capabilities for meta capability.
+		 */
+		public static function map_meta_cap( $caps, $cap ) {
+			if( 'upload_files' !== $cap )
+				return $caps;
+
+			return array( 'do_not_allow' );
+		}
+	}
 }
 
 BPC::hooks();
